5bcf8a0028c5f96ce77c49b6fa0562a688c9117e
Kacper <kputo77@gmail.com>
2025-04-08 10:33:58 +0200

    Add check if object is not null

--- src/Thingamajig/Scope.cxx
+++ src/Thingamajig/Scope.cxx
@@ -4,10 +4,13 @@ namespace webwork {
     Scope::Scope(const std::shared_ptr<properties::Object> &object, const std::shared_ptr<Scope> &previous) : object(object), previous(previous) {}
 
     std::shared_ptr<Property> Scope::GetProperty(const std::string &name) const {
-        const auto property = object->GetProperty(name);
-        if (property) {
-            return property;
+        if (object) {
+            const auto property = object->GetProperty(name);
+            if (property) {
+                return property;
+            }
         }
+
         if (previous) {
             return previous->GetProperty(name);
         }
