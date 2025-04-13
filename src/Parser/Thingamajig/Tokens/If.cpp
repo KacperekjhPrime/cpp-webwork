abe01303de9878578644560d5aa83cc62023a223
Kacper Puto <kputo77@gmail.com>
2025-04-13 09:55:46 +0200

    Remove completed TODO:

--- src/Parser/Thingamajig/Tokens/If.cpp
+++ src/Parser/Thingamajig/Tokens/If.cpp
@@ -8,7 +8,6 @@ namespace webwork::thingamajig {
     If::If(size_t textIndex, const std::shared_ptr<expression::Parenthesis> &expression) : Token(textIndex), BlockBase(TokenType::EndIf, "if"), expression(expression) {}
 
     std::string If::GetContent(const std::shared_ptr<Scope> &scope) const {
-        // TODO: Parse condition properly instead of treating it as a property name
         const auto result = expression->Evaluate(scope);
         const auto boolean = std::dynamic_pointer_cast<const properties::IBoolean>(result);
         if (!boolean) {
