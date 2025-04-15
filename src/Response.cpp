965ae53b3277002ca430b79e42b61b68a2b2bb8d
murczak <murczak0@interia.pl>
2025-04-15 20:37:46 +0200

    Begin work on setting cookies in responses

--- src/Response.cpp
+++ src/Response.cpp
@@ -1,9 +1,37 @@
 #include <cstring>
 #include <utility>
-#include <string>
 #include "Response.h"
 
 namespace webwork {
+    std::string Cookie::ToString() const {
+        std::vector<std::string> attributes;
+
+        if (domain != "") attributes.push_back("Domain=" + domain);
+        if (expires.has_value()) {
+            const std::chrono::zoned_time expiresGMT{"Etc/GMT", expires.value()};
+            const int seconds = std::stoi(std::format("{0:%S}", expires.value()));
+            attributes.push_back(std::format("Expires={0:%a}, {0:%d} {0:%m} {0:%Y} {0:%H}:{0:%M}:{1} GMT", expiresGMT, seconds));
+        }
+        if (httpOnly) attributes.push_back("HttpOnly");
+        if (maxAge.has_value()) attributes.push_back("Max-Age=" + maxAge.value());
+        if (partitioned) attributes.push_back("Partitioned");
+        if (path != "") attributes.push_back("Path=" + path);
+        if (secure) attributes.push_back("Secure");
+        if (sameSite != SameSite::Default) {
+            switch (sameSite) {
+                case SameSite::Strict:
+                    attributes.push_back("Strict");
+                    break;
+                case SameSite::Lax:
+                    attributes.push_back("Lax");
+                    break;
+                case SameSite::None:
+                    attributes.push_back("None");
+                    break;
+            }
+        }
+    }
+
     std::string Response::ToString() const {
         std::string result{};
 
--- src/Response.h
+++ src/Response.h
@@ -3,16 +3,39 @@
 #include <map>
 #include <string>
 #include <vector>
+#include <chrono>
 #include "StatusCodes.h"
 
 namespace webwork {
     const std::string HTTPVersion = "HTTP/1.0";
     const std::string Endline = "\r\n";
 
+    enum class SameSite {
+        Default = 0,
+        Strict = 1,
+        Lax = 2,
+        None = 3
+    };
+
+    struct Cookie {
+        std::string value;
+        std::string domain;
+        std::optional<std::chrono::sys_seconds> expires;
+        bool httpOnly = false;
+        std::optional<ssize_t> maxAge;
+        bool partitioned = false;
+        std::string path;
+        bool secure = false;
+        SameSite sameSite = SameSite::Default;
+
+        std::string ToString() const;
+    };
+
     struct Response {
         StatusCode code;
         std::vector<unsigned char> body;
         std::map<std::string, std::string> headers;
+        std::map<std::string, Cookie> cookies;
 
         std::string ToString() const;
     };
